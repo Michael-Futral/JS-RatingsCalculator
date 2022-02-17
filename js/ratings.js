40c867b7fc8ec8116f609e6a47a6b0aa4178075b
Michael Futral <Futralmc@dukes.jmu.edu>
2022-02-17 11:26:00 -0500

    -m

---
+++ js/ratings.js
@@ -0,0 +1,18 @@
+collect_ratings() {
+
+    let ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    };
+
+    let rating = 0;
+
+    let elements = document.querySelectorAll.rating;
+
+    array.forEach(element => {
+
+    });
+}
+
+
