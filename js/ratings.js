27d9aab8fccd9e0c799241effd1fd96dcdf65b32
Futral <FUTRALMC@ethyl.net>
2022-02-24 12:14:29 -0500

    -m

--- js/ratings.js
+++ js/ratings.js
@@ -1,20 +1,26 @@
 const { replaceWith } = require("cheerio/lib/api/manipulation");
 
-collect_ratings() {
+collect_ratings(); {
 
-    let ratings = {
+    const ratings = {
         count: 0,
         sum: 0,
         average: 0
     };
 
     let rating = 0;
-
-    let elements = document.querySelectorAll.rating;
-
-    array.forEach(element => {
-        replace()
+    const elements = document.querySelectorAll('.rating');
+    elements.forEach((element) => {
+        rating = parseInt(element.id.replace('star', ''));
+        ratings.count += parseInt(element.value);
+        ratings.sum += rating * parseInt(element.value);
     });
+
+    if (ratings.count !== 0) {
+        ratings.average = sum / count;
+    }
+
+    return ratings;
 };
 
 
